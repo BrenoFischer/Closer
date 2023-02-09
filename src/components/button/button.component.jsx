4e4055014e130f74c97239d3ddc753259e433a38
Breno Fischer <breno_perricone@hotmai.com>
2023-02-09 13:57:10 -0300

    add home header + initial Button

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,9 @@
+import './button.styles.scss';
+
+const Button = ({title}) => {
+    return(
+        <button className='button'>{title}</button>
+    );
+}
+
+export default Button;
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,8 +1,25 @@
-
+import Button from '../../components/button/button.component';
+import './home.styles.scss';
 
 const Home = () => {
+    const HomeHeader = () => {
+        return(
+            <header className="header">
+                <div className='header__container'>
+                    <h1 className='header__title'>A dedicated team to grow your company</h1>
+                    <p className='header__paragraph'>Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam neque ultrices.</p>
+                    <div className="buttons-container">
+                        <Button title="Social Enterprises" />
+                        <Button title="Corporates" />
+                        <Button title="Partnership" />
+                    </div>
+                </div>
+            </header>
+        );
+    }
+
     return(
-        <h1>Home</h1>
+        <HomeHeader />
     );
 }
 
