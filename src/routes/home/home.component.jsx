d622ce562d6680dda654f3eb6841687923df74a6
Breno Fischer <breno_perricone@hotmai.com>
2023-02-10 10:35:34 -0300

    add corporate section on Home

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -22,15 +22,15 @@ const Home = () => {
     
     const HomeEnterpriseSection = () => {
         return(
-            <section className='enterprise-section'>
-                <h2 className='enterprise-section__title'>Social Enterprise</h2>
-                <div className='enterprise-section__wrapper'>
-                    <div className='enterprise-section__img-container'>
-                        <div className='enterprise-section__icon-box'>
+            <section className='home-section enterprise-section'>
+                <h2 className='home-section__title'>Social Enterprise</h2>
+                <div className='home-section__wrapper'>
+                    <div className='home-section__img-container'>
+                        <div className='home-section__icon-box'>
                             <BsFillPlayCircleFill />
                         </div>
                     </div>
-                    <div className='enterprise-section__text-container'>
+                    <div className='home-section__text-container'>
                         <p>By creating a visual guide along the way, the designer or developer can get input from the other people involved in the website such as the customer, their manager, and other members of the team. </p>
                         <h3>Feature One</h3>
                         <p>By creating a visual guide along the way, the designer or developer can get input from the other people.</p>
@@ -44,20 +44,20 @@ const Home = () => {
 
     const HomeCorporateSection = () => {
         return(
-            <section className='corporate-section'>
-                <h2 className='corporate-section__title'>Corporates</h2>
-                <div className='corporate-section__wrapper'>
-                    <div className='corporate-section__img-container'>
-                        <div className='corporate-section__icon-box'>
-                            <BsFillPlayCircleFill />
-                        </div>
-                    </div>
-                    <div className='corporate-section__text-container'>
+            <section className='home-section corporate-section'>
+                <h2 className='home-section__title'>Corporates</h2>
+                <div className='home-section__wrapper'>
+                    <div className='home-section__text-container invert-text'>
                         <p>By creating a visual guide along the way, the designer or developer can get input from the other people involved in the website such as the customer, their manager, and other members of the team. </p>
                         <h3>Feature One</h3>
                         <p>By creating a visual guide along the way, the designer or developer can get input from the other people.</p>
                         <h3>Feature Two</h3>
                         <p>By creating a visual guide along the way, the designer or developer.</p>
+                    </div>
+                    <div className='home-section__img-container invert-img'>
+                        <div className='home-section__icon-box'>
+                            <BsFillPlayCircleFill />
+                        </div>
                     </div>
                 </div>
             </section>
@@ -69,6 +69,7 @@ const Home = () => {
             <HomeHeader />
             <main>
                 <HomeEnterpriseSection />
+                <HomeCorporateSection />
             </main>
         </>
     );
