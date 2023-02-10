4dfc775c734f5e5708bf528230600fcc944a4fca
Breno Fischer <breno_perricone@hotmai.com>
2023-02-10 10:38:28 -0300

    add partnerships section on Home

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -64,12 +64,35 @@ const Home = () => {
         );
     }
 
+    const HomePartnershipsSection = () => {
+        return(
+            <section className='home-section partsnerships-section'>
+                <h2 className='home-section__title'>Partnerships</h2>
+                <div className='home-section__wrapper'>
+                    <div className='home-section__img-container'>
+                        <div className='home-section__icon-box'>
+                            <BsFillPlayCircleFill />
+                        </div>
+                    </div>
+                    <div className='home-section__text-container'>
+                        <p>By creating a visual guide along the way, the designer or developer can get input from the other people involved in the website such as the customer, their manager, and other members of the team. </p>
+                        <h3>Feature One</h3>
+                        <p>By creating a visual guide along the way, the designer or developer can get input from the other people.</p>
+                        <h3>Feature Two</h3>
+                        <p>By creating a visual guide along the way, the designer or developer.</p>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+
     return(
         <>
             <HomeHeader />
             <main>
                 <HomeEnterpriseSection />
                 <HomeCorporateSection />
+                <HomePartnershipsSection />
             </main>
         </>
     );
