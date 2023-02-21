c8f226c0b3afb3e0adec5a177abce569afe05cae
Breno Fischer <breno_perricone@hotmai.com>
2023-02-20 21:54:51 -0300

    add about description section - desktop

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -1,6 +1,7 @@
 import Footer from '../../components/footer/footer.components';
 
 import AboutImg from '../../assets/about-header-img.png';
+import Logo from '../../assets/logo-color.png';
 
 import './about.styles.scss';
 
@@ -19,10 +20,32 @@ const About = () => {
         );
     }
 
+    const AboutDescription = () => {
+        return(
+            <main className='about-description'>
+                <div className='about-description__wrapper'>
+                    <h3 className='about-description__title'>What is Closer?</h3>
+                    <div className='about-description__paragraph-wrapper'>
+                        <p className='about-description__paragraph'><b>CLOSER</b> is bringing the social enterprise community together - making connections and sharing stories, experiences and ideas.</p>
+                        <br/>
+                        <br/>
+                        <p className='about-description__paragraph'> We think you're going to get something special from being a part of the Social Impact Ireland online community hub. Part of our role is to bring this unique community together, to create for themselves, guided by themselves, a strong community - one where each is respected for who they are, where learnings can be shared, mutual support offered, successes shared and difficult moments eased.</p>
+                        <br/>
+                        <br/>
+                        <p className='about-description__paragraph'>Join <b>CLOSER</b> today and say Hello to the social enterprise community around Ireland and further.</p>
+                    </div>
+                </div>
+                <div>
+                    <img className='about-description__img' src={Logo} alt="Closer Logo" />
+                </div>
+            </main>
+        );
+    }
+
     return(
         <>
             <AboutHeader />
-            <div>Espaco</div>
+            <AboutDescription />
             <Footer />
         </>
     );
