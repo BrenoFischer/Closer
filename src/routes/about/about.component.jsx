ce7e49735760e1a94f1cb37f16c03815f53a9576
Breno Fischer <breno_perricone@hotmai.com>
2023-02-20 21:22:09 -0300

    add about header desktop

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -1,11 +1,28 @@
 import Footer from '../../components/footer/footer.components';
 
+import AboutImg from '../../assets/about-header-img.png';
+
 import './about.styles.scss';
 
 const About = () => {
+    const AboutHeader = () => {
+        return(
+            <header className='about-header'>
+                <div className='about-header__text-container'>
+                    <h2 className='about-header__title'>Welcome to Closer</h2>
+                    <div className='about-header__paragraph-wrapper'>
+                        <p className='about-header__paragraph'>Your online hub bringing the social enterprise community together</p>
+                    </div>
+                </div>
+                <img className='about-header__img' src={AboutImg} alt="About" />
+            </header>
+        );
+    }
+
     return(
         <>
-            <h1>About</h1>
+            <AboutHeader />
+            <div>Espaco</div>
             <Footer />
         </>
     );
