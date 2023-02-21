4d887234835d6ac13c5b1450ecf44195558f36bf
Breno Fischer <breno_perricone@hotmai.com>
2023-02-21 12:23:46 -0300

    add about video

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -42,10 +42,24 @@ const About = () => {
         );
     }
 
+    const YoutubeEmbed = () => (
+        <div className="about-video">
+            <iframe 
+                width="560" 
+                height="315"
+                src="https://www.youtube.com/embed/j-fVMDp0rZA" 
+                title="Closer video player" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowFullScreen>
+            </iframe>
+        </div>
+    );
+
     return(
         <>
             <AboutHeader />
             <AboutDescription />
+            <YoutubeEmbed />
             <Footer />
         </>
     );
