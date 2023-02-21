2759e1b1aac2d46950b59344f35448d71dc98316
Breno Fischer <breno_perricone@hotmai.com>
2023-02-21 13:16:09 -0300

    add about reports section

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -2,6 +2,8 @@ import Footer from '../../components/footer/footer.components';
 
 import AboutImg from '../../assets/about-header-img.png';
 import Logo from '../../assets/logo-color.png';
+import ReportImg1 from '../../assets/reports-img.png';
+import ReportImg2 from '../../assets/reports-img2.png';
 
 import './about.styles.scss';
 
@@ -55,11 +57,46 @@ const About = () => {
         </div>
     );
 
+    const Reports = () => (
+        <section className='about-reports'>
+            <div className='about-reports__header'>
+                <h3 className='about-reports__title'>What they say</h3>
+                <p className='about-reports__subtitle'>a word from some of our members</p>
+            </div>
+            <div className='about-reports__wrapper'>
+                <div className='about-reports__img-container'>
+                    <img className='about-reports__img' src={ReportImg1} />
+                    <div className='about-reports__img-label'>
+                        <p>Mamobo Ogoro</p>
+                        <p>Gorm Media</p>
+                    </div>
+                </div>
+                <div className='about-reports__report-text'>
+                    <p>“Closer has been a great tool for linking in with other Social Entrepreneurs and hearing opportunities to further the mission of my social enterprise”</p>
+                </div>
+            </div>
+
+            <div className='about-reports__wrapper'>
+                <div className='about-reports__report-text inverted-report'>
+                    <p>“As a solo social entrepreneur, I find the support from like-minded people on a very similar journey invaluable. The updates, links, funding opportunity news, and general chat are so useful”</p>
+                </div>
+                <div className='about-reports__img-container'>
+                    <img className='about-reports__img' src={ReportImg2} />
+                    <div className='about-reports__img-label'>
+                        <p>Rachel Tyrell</p>
+                        <p>The Colour Club</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+
     return(
         <>
             <AboutHeader />
             <AboutDescription />
             <YoutubeEmbed />
+            <Reports />
             <Footer />
         </>
     );
