71d61365163354b71677977f72a01e26bab04546
Breno Fischer <breno_perricone@hotmai.com>
2023-03-03 12:59:48 -0300

    initial design for nav burger menu - phone navbar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,6 +6,18 @@ import Logo from '../../assets/logo-color.png';
 
 import './navigation.styles.scss';
 
+
+const LinksList = () => (
+    <>
+        <Link className='navigation__link' to='/'>Home</Link>
+        <Link className='navigation__link' to='/about'>About Closer</Link>
+        <Link className='navigation__link' to='/'>Social Enterprise</Link>
+        <Link className='navigation__link' to='/'>Corporates</Link>
+        <Link className='navigation__link' to='/'>Partnership</Link>
+        <Link className='navigation__link' to='/contact'>Contact</Link>
+    </>
+)
+
 const Navigation = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
@@ -20,35 +32,31 @@ const Navigation = () => {
                     <img src={Logo} alt='Logo' />
                 </Link>
                 <div className='navigation__burger-menu' onClick={toggleMenu}>
-                    {menuOpen ? (
-                        <>
-                            <span className='bar'></span>
-                            <span className='bar'></span>
-                            <span className='bar'></span>
-                        </>
-                    ):
-                    (
-                        <div className='navigation__menu-open'>
+                    {menuOpen ?
+                        (<>
                             <span className='close-bar'></span>
                             <span className='close-bar-bottom'></span>
-                            {/* <ul className='navigation__links-container--phone'>
-                                <Link className='navigation__link' to='/'>Home</Link>
-                                <Link className='navigation__link' to='/'>About Closer</Link>
-                                <Link className='navigation__link' to='/'>Social Enterprise</Link>
-                                <Link className='navigation__link' to='/'>Corporates</Link>
-                                <Link className='navigation__link' to='/'>Partnership</Link>
-                                <Link className='navigation__link' to='/'>Contact</Link>
-                            </ul> */}
-                        </div>
-                    )}
+                        </>)
+                    :
+                        (<>
+                            <span className='bar'></span>
+                            <span className='bar'></span>
+                            <span className='bar'></span>    
+                        </>)
+                    }
                 </div>
+                {menuOpen ?
+                    (<div className='navigation__menu-open'>
+                        <ul className='navigation__links-container--phone'>
+                            <LinksList />
+                        </ul> 
+                    </div>)
+                :
+                    (<></>)
+                }
+
                 <ul className='navigation__links-container'>
-                    <Link className='navigation__link' to='/'>Home</Link>
-                    <Link className='navigation__link' to='/about'>About Closer</Link>
-                    <Link className='navigation__link' to='/'>Social Enterprise</Link>
-                    <Link className='navigation__link' to='/'>Corporates</Link>
-                    <Link className='navigation__link' to='/'>Partnership</Link>
-                    <Link className='navigation__link' to='/contact'>Contact</Link>
+                    <LinksList />
                 </ul>
             </nav>
             <Outlet />
